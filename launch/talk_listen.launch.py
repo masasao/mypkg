a8583c89d56a6784e3b4d450b206045bc4524a05
masasao <ninjinuragosi@gmail.com>
2023-02-19 11:02:03 +0900

    doo

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+# SPDX-FileCopyrightText: 2023 masasao　　　　　
+# SPDX-License-Identifier: BSD-3-Clause
+
 import launch
 import launch.actions
 import launch.substitutions
@@ -7,12 +11,12 @@ import launch_ros.actions
 def generate_launch_description():
        
     talker = launch_ros.actions.Node(
-        package='mypkg',
-        node_executable='talker',
-        )
+            package='mypkg',
+            node_executable='talker',
+            )
     listener = launch_ros.actions.Node(
-        package='mypkg',
-        node_executable='listener',
-        output='screen'
-                       )
+            package='mypkg',
+            node_executable='listener',
+            output='screen'
+            )
     return launch.LaunchDescription([talker, listener])
